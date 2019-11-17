fb5834b07af5d2b49986cd2eef40f1f2c8def317
sijmenhuizenga <sijmenhuizenga@gmail.com>
2019-11-17 03:03:13 +0100

    blablabuttons

--- turnoff/src/App.js
+++ turnoff/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'rc-slider/assets/index.css';
 import Moment from 'moment';
 import Slider from 'rc-slider';
+import ToggleButton from '@material-ui/lab/ToggleButton';
 
 const startingDate = Moment("1996-1-1");
 const amountOfImages = 5;
@@ -9,8 +10,9 @@ const autoplayTimeout = 50;
 
 const imageindex = [
     {prefix: "tempplots/temp", blendmode: "normal"},
-    {image: "/images/heightmap-akatopo.png", blendmode: "normal", opacity: 0.4},
-    {prefix: "riverplots/river", blendmode: "screen", opacity: 0.5},
+    {image: "/images/heightmap-akatopo.png", blendmode: "normal", opacity: 0.4, togglevar: "enabledHeithmap"},
+    {prefix: "riverplots/river", blendmode: "screen", opacity: 0.5, togglevar: "enabledRivers"},
+    {image: "/images/borderplot/border.png", blendmode: "normal", togglevar: "enabledBorders"}
 ];
 
 class App extends React.Component {
@@ -20,6 +22,9 @@ class App extends React.Component {
         this.state = {
             imageNumber: 1,
             autoplayEnabled: false,
+            enabledHeithmap: true,
+            enabledRivers: true,
+            enabledBorders: true,
         };
         setInterval(this.handleAutoplayTimeout.bind(this), autoplayTimeout);
     }
@@ -32,6 +37,18 @@ class App extends React.Component {
                 </div>
                 <div className="imagecontainer" onClick={this.toggleAutoplay.bind(this)} dangerouslySetInnerHTML={{__html: this.renderSvgAsText()}} />
 
+                <div className="aaaah">
+                    <ToggleButton value="check" selected={this.state.enabledHeithmap} onChange={this.toggle("enabledHeithmap")}>
+                        Toggle Heightmap
+                    </ToggleButton> &nbsp;
+                    <ToggleButton value="check" selected={this.state.enabledRivers} onChange={this.toggle("enabledRivers")}>
+                        Toggle Rivers
+                    </ToggleButton>&nbsp;
+                    <ToggleButton value="check" selected={this.state.enabledBorders} onChange={this.toggle("enabledBorders")}>
+                        Toggle Borders
+                    </ToggleButton>
+                </div>
+
                 <span className="metadata">
                     {Moment(startingDate).add(this.state.imageNumber - 1, "day").format("DD MMM YYYY")}
                 </span>
@@ -39,10 +56,17 @@ class App extends React.Component {
         );
     }
 
+    toggle(vari) {
+        return () => {this.setState({[vari]: !this.state[vari]})}
+    }
+
     renderSvgAsText() {
         let output = "<svg style='width: 780px; height: 800px'>";
 
         imageindex.forEach((imagething) => {
+            if(imagething.hasOwnProperty("togglevar") && !this.state[imagething.togglevar]) {
+                return;
+            }
             if(imagething.hasOwnProperty("image")) {
                 output += `<image href="${imagething.image}" style="mix-blend-mode: ${imagething.blendmode}; opacity: ${imagething.opacity || 1}" />`;
             } else {
