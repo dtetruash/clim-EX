eebe4752d3b5bb67ad12dd5c4a9a60884055cb80
tommylees112 <thomas.lees112@gmail.com>
2019-11-17 00:06:22 +0000

    interpolate script

--- preprocessing/regrid_interp.py
+++ preprocessing/regrid_interp.py
@@ -2,11 +2,16 @@ import xarray as xr
 from pathlib import Path
 
 # load datasets into memory
-reference_nc_filepath = Path('')
+data_dir = Path('')
+
+reference_nc_filepath = data_dir / 'precip_doy.nc'
 reference_nc = xr.open_dataset(reference_nc_filepath)
 
-filepath = Path('')
+filepath = data_dir / 'precip_doy.nc'
 ds = xr.open_dataset(filepath)
+
+output_path = data_dir / 'output.nc'
 
 # use xarray interpolate
 ds = ds.interp(lon=reference_nc.lon, lat=reference_nc.lat)
+
